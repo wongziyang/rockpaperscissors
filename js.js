a0c25ca7f1b563d83cf090ad3beb5c7d3bd73c08
Wong Ziyang <wongziyang1996@gmail.com>
2022-05-07 23:01:52 +0800

    Completed playRound() function

--- js.js
+++ js.js
@@ -4,11 +4,32 @@ function computerPlay() {
     return selection[idx];
 }
 
-function oneRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection){
+    let win = 0;
+
     // Capitalize first letter and ensures the rest of the letters are lower case
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     
-    
+    if(playerSelection==='Rock' && computerSelection==='Scissors'){
+        win = 2;
+    }else if(playerSelection==='Rock' && computerSelection==='Rock'){
+        win = 1;
+    }else if(playerSelection==='Paper' && computerSelection==='Rock'){
+        win = 2;
+    }else if(playerSelection==='Paper' && computerSelection==='Paper'){
+        win = 1;
+    }else if(playerSelection==='Scissors' && computerSelection==='Paper'){
+        win = 2;
+    }else if(playerSelection==='Scissors' && computerSelection==='Scissors'){
+        win = 1;
+    }
 
-
+    if(win===2){
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }else if(win===1){
+        return `Draw!`;
+    }else{
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
 }
+
